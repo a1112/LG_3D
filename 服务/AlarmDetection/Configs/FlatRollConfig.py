1e96bef41ee4d9ec4c88bf78908f244319178b6c
move_pc <1042815796@qq.com>
2025-04-24 22:49:31 +0800

    add

--- 服务/AlarmDetection/Configs/FlatRollConfig.py
+++ 服务/AlarmDetection/Configs/FlatRollConfig.py
@@ -13,26 +13,39 @@ class FlatRollConfig(ConfigBase):
     """
     def __init__(self, config,data_integration:DataIntegration):
         test = {
-            "Base": {
-                "name": "默认判断规则",
-                "max": 780,
-                "min": 600,
-                "info": "默认判断规则 最大为780，最大最小差别≤50mmm  最小设置为 600 mm"
-            },
-            "0": {
-                "name": "VAMA",
-                "max": 780,
-                "min": 705,
-                "info": "冷轧去向如有松卷(12点方向两层之间超过6mm认定为松卷)，不计最内圈应大于715mm，包含最内圈需不低于700mm"
-            },
-            "2": {
-                "name": "冷轧基板",
-                "min": 700,
-                "max": 780,
-                "info": "冷轧去向如有松卷(12点方向两层之间超过6mm认定为松卷)，不计最内圈应大于715mm，包含最内圈需不低于700mm"
-            }
-        }
+            # "Base": {
+            #     "name": "默认判断规则",
+            #     "max": 780,
+            #     "min": 600,
+            #     "info": "默认判断规则 最大为780，最大最小差别≤50mmm  最小设置为 600 mm"
+            # },
+            # "0": {
+            #     "name": "VAMA",
+            #     "max": 780,
+            #     "min": 705,
+            #     "info": "冷轧去向如有松卷(12点方向两层之间超过6mm认定为松卷)，不计最内圈应大于715mm，包含最内圈需不低于700mm"
+            # },
+            # "2": {
+            #     "name": "冷轧基板",
+            #     "min": 700,
+            #     "max": 780,
+            #     "info": "冷轧去向如有松卷(12点方向两层之间超过6mm认定为松卷)，不计最内圈应大于715mm，包含最内圈需不低于700mm"
+            # }
+            "msg":"塔形检测",
+            "configs":[
+                {
+                    "filter":{
+                        "type":"base"
+                    }
 
+
+
+                }
+
+
+            ]
+        }
+        config = test
         super().__init__(config)
         self.data_integration = data_integration
 
